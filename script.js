8ca8ee1868e4957192f01b9374435d6bfd8bae0f
RobertEnagnon <80330728+RobertEnagnon@users.noreply.github.com>
2024-10-14 19:50:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 async function fetchImages() {
     try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/photos?_limit=10');
+        // const response = await fetch('https://jsonplaceholder.typicode.com/photos?_limit=10');
+        const response = await fetch('https://dog.ceo/api/breeds/image/random/10');
         const images = await response.json();
-        displayImages(images);
+        console.log(images);
+        // displayImages(images);
+        displayImages(images.message);
     } catch (error) {
         console.error('Erreur lors du fetch:', error);
     }
@@ -12,7 +15,8 @@ function displayImages(images) {
     const gallery = document.getElementById('gallery');
     images.forEach(image => {
         const imgElement = document.createElement('img');
-        imgElement.src = image.url;
+        // imgElement.src = image.url;
+        imgElement.src = image;
         gallery.appendChild(imgElement);
     });
 }
